b0a51186e35e321bfb1aee1f3980fe80e8e0244b
Thiago Langsdorf <langsdorf@outlook.com.br>
2021-11-26 20:16:58 -0300

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,80 @@
+## Uso
+
+### Deploy
+
+Instale as dependências:
+
+```
+npm install
+```
+
+Depois, rode o comando:
+
+```
+npm run deploy
+```
+
+Será gerado o arquivo JavaScript e iniciará o deploy para AWS com o Serverless Framework.
+
+> Talvez seja necessário realizar o login no console da AWS
+> A as configurações do DynamoDB e outras estão no arquivo `serverless.yml`
+
+Após o deploy, será mostrado a URL pública da aplicação que está liberada para todos por padrão (sem auth)
+
+**A região padrão é us-east-1**
+
+### Chamar API
+
+Após o deploy, os seguintes endpoints estarão disponíveis:
+
+__Substitua o xxxxxx pelo nome gerado no deploy (será mostrado no console após a realização dos passos acima)__
+
+#### Criar funcionário
+
+`POST https://xxxxxx.execute-api.us-east-1.amazonaws.com/employees`
+
+```bash
+curl -X POST "https://xxxxxx.execute-api.us-east-1.amazonaws.com/employees" -H "Content-Type: application/json" --data-raw '{"id": 1, "name": "Nome", "age": 0, "role": "admin"}'
+```
+
+#### Listar todos os funcionários
+
+`GET https://xxxxxx.execute-api.us-east-1.amazonaws.com/employees`
+
+```bash
+curl https://xxxxxxx.execute-api.us-east-1.amazonaws.com/employees
+```
+
+#### Pegar informações de um funcionário
+
+`GET https://xxxxxx.execute-api.us-east-1.amazonaws.com/employees/:uuid`
+
+```bash
+export UUID=uuid && curl "https://xxxxxxx.execute-api.us-east-1.amazonaws.com/employees/$UUID" && unset UUID
+```
+
+#### Deletar um funcionário
+
+`DELETE https://xxxxxx.execute-api.us-east-1.amazonaws.com/employees/:uuid`
+
+```bash
+export UUID=uuid && curl -X DELETE "https://xxxxxxx.execute-api.us-east-1.amazonaws.com/employees/$UUID" && unset UUID
+```
+
+#### Atualizar um funcionário
+
+`PUT https://xxxxxx.execute-api.us-east-1.amazonaws.com/employees/:uuid`
+
+```bash
+export UUID=uuid && curl -X PUT "https://xxxxxx.execute-api.us-east-1.amazonaws.com/employees/$UUID" -H 'Content-Type: application/json' --data-raw '{"id": 2, "name": "Nome2", "age": 1, "role": "admin2"}' && unset UUID
+```
+
+## Limpeza
+
+Para remover o que foi criado na AWS, execute o comando:
+
+```bash
+npm run clear
+```
+
+> Será deletada a pilha criada pelo Serverless, juntamente com a tabela do DynamoDB.
---
+++ main.ts
@@ -0,0 +1,218 @@
+import express from "express";
+import ServerlessHttp from "serverless-http";
+import AWS, { AWSError, DynamoDB } from "aws-sdk";
+import { PromiseResult } from "aws-sdk/lib/request";
+import {
+  DeleteItemOutput,
+  GetItemOutput,
+  PutItemOutput,
+  ScanOutput,
+  UpdateItemOutput,
+} from "aws-sdk/clients/dynamodb";
+import { v4 as uuid } from "uuid";
+
+const app = express();
+app.use(express.json());
+
+const EMPLOYEES_TABLE = process.env.EMPLOYEES_TABLE;
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+
+/**
+ * @api {post} /employees Create an employee
+ */
+app.post("/employees", async (req, res) => {
+  const { id, name, age, role } = req.body;
+
+  if (checkFields(id, name, age, role))
+    return generateResponse(res, 400, "Bad Request");
+
+  try {
+    const body = {
+      uuid: uuid(),
+      id,
+      name,
+      age,
+      role,
+    };
+    
+    const result = await dynamoDb({
+      method: "put",
+      params: {
+        Item: body,
+      },
+    });
+
+    generateResponse(res, 200, body);
+  } catch (e) {
+    generateResponse(res, 500, "Could not create employees", e);
+  }
+});
+
+/**
+ * @api {delete} /employees/:id Deletes an employee
+ */
+app.delete("/employees/:uuid", async (req, res) => {
+  try {
+    const result = await dynamoDb({
+      method: "delete",
+      params: {
+        Key: {
+          uuid: req.params.uuid,
+        },
+      },
+    });
+
+    generateResponse(res, 200, result);
+  } catch (e) {
+    generateResponse(res, 500, "Could not delete employees", e);
+  }
+});
+
+/**
+ * @api {get} /employees Get all employees
+ */
+app.get("/employees", async (req, res) => {
+  try {
+    const result = await dynamoDb({
+      method: "scan",
+      params: {},
+    });
+
+    generateResponse(res, 200, result.Items);
+  } catch (e) {
+    generateResponse(res, 500, "Could not get employees", e);
+  }
+});
+
+/**
+ * @api {get} /employees/:id Get an employee
+ */
+app.get("/employees/:uuid", async (req, res) => {
+  try {
+    const result = await dynamoDb({
+      method: "get",
+      params: {
+        Key: {
+          uuid: req.params.uuid,
+        },
+      },
+    });
+
+    generateResponse(res, 200, result.Item);
+  } catch (e) {
+    generateResponse(res, 500, "Could not get employee", e);
+  }
+});
+
+/**
+ * @api {put} /employees/:id Update an employee
+ */
+app.put("/employees/:uuid", async (req, res) => {
+  const { id, name, age, role } = req.body;
+
+  if (checkFields(id, name, age, role))
+    return generateResponse(res, 400, "Bad Request");
+
+  try {
+    const result = await dynamoDb({
+      method: "update",
+      params: {
+        Key: {
+          uuid: req.params.uuid,
+        },
+        UpdateExpression:
+          "set id = :id, name = :name, age = :age, role = :role",
+        ExpressionAttributeValues: {
+          ":id": req.body.id,
+          ":name": req.body.name,
+          ":age": req.body.age,
+          ":role": req.body.role,
+        },
+        ReturnValues: "UPDATED_NEW",
+      },
+    });
+
+    generateResponse(res, 200, result);
+  } catch (e) {
+    generateResponse(res, 500, "Could not update employee", e);
+  }
+});
+
+/**
+ * Listen all invalid routes
+ */
+app.all("*", (req, res) => {
+  res.status(404).json({
+    statusCode: 404,
+    message: `${req.path} not found`,
+  });
+});
+
+export const handler = ServerlessHttp(app);
+
+/**
+ * Check if all fields are present
+ */
+const checkFields = (...body: any[]) => {
+  return !body.some((x) => x !== null && x !== "");
+};
+
+/**
+ * Generate response
+ */
+const generateResponse = (
+  res: express.Response,
+  statusCode: number,
+  content: any,
+  error?: any
+) => {
+  if (error) console.error(error);
+
+  return res.status(statusCode).json({
+    statusCode,
+    message: content,
+  });
+};
+
+/**
+ * Makes easier to use dynamoDb
+ */
+async function dynamoDb<T extends DynamoDBParams>({
+  method,
+  params,
+}: T): Promise<PromiseResult<DynamoDbReturnType<T>, AWSError>> {
+  try {
+    const result = await dynamoDbClient[method]({
+      TableName: EMPLOYEES_TABLE,
+      ...params,
+    }).promise();
+
+    return result as PromiseResult<DynamoDbReturnType<T>, AWSError>;
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
+
+/**
+ * Typescript stuff
+ */
+type Update = "update";
+type Get = "get";
+type Scan = "scan";
+type Delete = "delete";
+type Put = "put";
+
+type DynamoDBMethod = Update | Get | Scan | Delete | Put;
+type DynamoDBParams = { method: DynamoDBMethod; params: any };
+
+type DynamoDbReturnType<T extends DynamoDBParams> = T["method"] extends Update
+  ? UpdateItemOutput
+  : T["method"] extends Get
+  ? GetItemOutput
+  : T["method"] extends Scan
+  ? ScanOutput
+  : T["method"] extends Delete
+  ? DeleteItemOutput
+  : T["method"] extends Put
+  ? PutItemOutput
+  : never;
